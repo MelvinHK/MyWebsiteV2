78a1c4428d41aad36da810806d51a9ee775f3788
Melvin <melvinhkusuma@gmail.com>
2024-08-26 18:49:06 +1000

    bookmark button simply goes to title screen now

--- main.js
+++ main.js
@@ -23,8 +23,6 @@ barba.init({
 
 function handleNav(data) {
   const nav = document.getElementById('bookmark');
-  const menu = document.getElementById('bookmark-menu');
-
   const nextUrl = data.next.url.path;
   const isHome = (nextUrl === '/' || nextUrl === '/index.html');
 
@@ -34,25 +32,10 @@ function handleNav(data) {
     gsap.to(nav, {
       translateY: isHome ? '0px' : '100px',
       ease: `power1.${isHome ? 'in' : 'out'}`,
-    }),
-    gsap.to(menu, {
-      opacity: 0,
-      pointerEvents: "none"
     })
   ];
 }
 
-document.getElementById('bookmark').addEventListener('click', (e) => {
-  const nav = e.currentTarget;
-  const menu = document.getElementById('bookmark-menu');
-  const isOpen = gsap.getProperty(nav, 'y') !== 100;
-
-  gsap.to(menu, {
-    opacity: isOpen ? 0 : 1,
-    pointerEvents: isOpen ? "none" : "all"
-  });
-
-  gsap.to(nav, {
-    translateY: isOpen ? 100 : '250%'
-  });
+document.getElementById('bookmark').addEventListener('click', () => {
+  
 });
