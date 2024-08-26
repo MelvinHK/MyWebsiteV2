9ce93da802371ca8a279ae62d4df00cf544101fc
Melvin <melvinhkusuma@gmail.com>
2024-08-26 14:36:58 +1000

    added bookmark nav button

--- main.js
+++ main.js
@@ -7,10 +7,26 @@ barba.init({
     leave(data) {
       return gsap.to(data.current.container, {
         opacity: 0,
-        translateX: "-5rem",
+        translateX: '-5rem',
         duration: 0.25,
-        ease: "power1.in"
+        ease: 'power1.in'
       });
+    },
+    enter(data) {
+      return getNavTransition(data.next.url.path);
+    },
+    once(data) {
+      return getNavTransition(data.next.url.path);
     }
   }],
 });
+
+function getNavTransition(path) {
+  const nav = document.getElementsByClassName('bookmark');
+  const isHome = (path === '/' || path === '/index.html');
+
+  return gsap.to(nav, {
+    translateY: isHome ? '0px' : '100px',
+    ease: `power1.${isHome ? 'in' : 'out'}`,
+  });
+}
